1faf3ab3dd89a618d155a247c7cc4fc6f592fb4a
aydnlramazan <106530954+aydnlramazan@users.noreply.github.com>
2023-11-19 20:53:04 +0300

    add drawer and pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,179 @@
+/*import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Anasayfa());
+}
+
+class Anasayfa extends StatelessWidget {
+  const Anasayfa({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Stateless Sayfa Örneği"),
+        ),
+        body: const Center(
+          child: Text(
+            "Merhaba Dünya",
+            style: TextStyle(fontSize: 45),
+          ),
+        ),
+      ),
+    );
+  }
+}
+*/
+/*
+
+import 'package:flutter/material.dart';
+
+//ilk yapılması gereken işlemler void main içinde işlemler yapılır
+void main() {
+  runApp(
+    const MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Flutter Örnek",
+      theme: ThemeData(primarySwatch: Colors.red),
+      home: MyHomePage(
+        title: "FLUTTER ÖRNEK",
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  //const MyHomePage({super.key, required this.title});
+
+  String title = "";
+
+  MyHomePage({super.key, required this.title});
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  void initState() {
+    print("ilgili class'ın init state metodudur");
+    super.initState();
+  }
+
+  int _counter = 0;
+
+  void _incrementCounter() {
+    //koza container verileri burda saklıyoruz, değiştiryoruz.
+    setState(() {
+      _counter++;
+    });
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            const Text("bu botana kaç kez tıklandı"),
+            Text(
+              "$_counter",
+              style: Theme.of(context).textTheme.headlineLarge,
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        child: const Icon(Icons.add),
+        tooltip: "sayıyı artırır",
+      ),
+    );
+  }
+}
+*/
+/*
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const TabBarApp());
+}
+
+class TabBarApp extends StatelessWidget {
+  const TabBarApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: TabBarOrnek(),
+    );
+  }
+}
+
+class TabBarOrnek extends StatelessWidget {
+  const TabBarOrnek({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      initialIndex: 0,
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          
+          title: const Text("----------- tapbar -------------"),
+          bottom: const TabBar(tabs: <Widget>[
+            Tab(
+              icon: Icon(
+                Icons.cloud_outlined,
+              ),
+            ),
+            Tab(
+              icon: Icon(
+                Icons.padding,
+              ),
+            ),
+            Tab(
+              icon: Icon(
+                Icons.do_disturb,
+              ),
+            ),
+            Tab(
+              icon: Icon(
+                Icons.high_quality,
+              ),
+            ),
+          ]),
+        ),
+        body: const TabBarView(children: <Widget>[
+          Center(
+            child: Text("bugün hava bulutlu"),
+          ),
+          Center(
+            child: Text("bugün hava güneşli"),
+          ),
+          Center(
+            child: Text("bugün hava karlı"),
+          ),
+          Center(
+            child: Text("bugün hava fırtınalı"),
+          ),
+        ]),
+      ),
+    );
+  }
+}*/
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -5,121 +181,53 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
+  //metodun geri dönüş tipi widget olduğu için return ile
+  //dönmemmiz gereken widget ne ise onu bu metod ile döndermeliyiz.
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      home: NagigasyonOrnek(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class NagigasyonOrnek extends StatelessWidget {
+  const NagigasyonOrnek({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text("Navigasyonumuz <^>"),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+      body: const Center(
+        child: Text("burası ekranımızın body'si"),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      drawer: Drawer(
+        child: ListView(children: const <Widget>[
+          UserAccountsDrawerHeader(
+            accountName: Text("ramazan aydınlı"),
+            accountEmail: Text("dytramazanaydinli@gmail.com"),
+            currentAccountPicture: CircleAvatar(
+              backgroundImage: AssetImage("images/ra.jpg"),
+            ),
+          ),
+          ListTile(
+            title: Text("Anasayfa"),
+            leading: Icon(Icons.home),
+          ),
+          ListTile(
+            title: Text("Profil"),
+            leading: Icon(Icons.person),
+          ),
+          ListTile(
+            title: Text("Bilgi"),
+            leading: Icon(Icons.info_sharp),
+          ),
+        ]),
+      ),
     );
   }
 }
